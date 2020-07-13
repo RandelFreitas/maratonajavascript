6a47a3b49051a535cb94ac8db5449c1d1b96faae
randel <randelfreitas@gmail.com>
2020-07-13 16:24:41 -0300

    Store accounts into database

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,12 +1,25 @@
 const express = require ('express');
+const bcrypt = require('bcrypt');
+const { Account } = require('../models');
 
 const router = express.Router();
+
+const saltRounds = 10;
 
 router.get('/sign-in', (req, res)=>{
     return res.json('Sign in!');
 });
 
-router.get('/sign-up', (req, res)=>{
+router.get('/sign-up', async (req, res)=>{
+    const email = 'randel@randel.com';
+    const password = '123456';
+
+    //const salt = 'querysdaas';
+    const hash = bcrypt.hashSync(password, saltRounds);
+
+    //const result = await Account.create({email, password});
+    console.log(hash);
+
     return res.json('Sign up!');
 });
 
